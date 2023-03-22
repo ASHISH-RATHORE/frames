f0c29639d3bd92ca88c7c5d76c8295625bc7a295
ashish <ashishr97@gmail.com>
2023-03-22 23:38:47 +0530

    changes in ui

--- src/pages/auth/register.js
+++ src/pages/auth/register.js
@@ -13,7 +13,16 @@ function Page() {
   return (
     <>
      <Head>
-     <meta name="description" content={"Join the frames community for amazing content"} />
+     <meta property="og:title" content="European Travel Destinations"/>
+<meta property="og:type" content="article" />
+<meta property="og:image" content="http://euro-travel-example.com/thumbnail.jpg"/>
+<meta property="og:url" content="http://euro-travel-example.com/index.htm"/>
+<meta name="twitter:card" content="summary_large_image"/>
+
+{/* <!--  Non-Essential, But Recommended --> */}
+<meta property="og:description" content="Offering tour packages for individuals or groups."/>
+<meta property="og:site_name" content="European Travel, Inc."/>
+<meta name="twitter:image:alt" content="Alt text for image"/>
         <title>
           Register | Frames
         </title>
